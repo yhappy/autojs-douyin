587254ca871ac102f4727acdae807df19977dbd5
yhappy <yhappy_2008@126.com>
2020-03-19 19:40:08 +0800

    add ui

---
+++ ui.js
@@ -0,0 +1,15 @@
+"ui";
+ui.layout(
+    <frame>
+        <vertical>
+            <appbar>
+                <toolbar id="toolbar" title="Dytools"></toolbar>
+            </appbar>
+            <linear margin="10">
+                <text textSize="16sp">sex</text>
+                <spinner id="sp1" entries="-man-|-woman-"></spinner>
+            </linear>
+            <button id="start">start</button>
+        </vertical>P
+    </frame>
+);
